94e6255e25595ab21be0ab18adac5b2786a52750
Argun Cankat Ergün <arguncankatergun@gmail.com>
2025-11-06 18:32:41 +0300

    RFM analysis output file added and classification analysis updated

--- classification_analysis.py
+++ classification_analysis.py
@@ -1,14 +1,15 @@
 # User-OAuth BigQuery connect + smoke test
 
-import pydata_google_auth
-from google.cloud import bigquery
-import pandas_gbq as gbq
+#import pydata_google_auth
+#from google.cloud import bigquery
+#import pandas_gbq as gbq
 
-PROJECT_ID = "uk-online-retail-data-project"   # <-- confirm your project id
-SCOPES = [
-    "https://www.googleapis.com/auth/bigquery",
-    "https://www.googleapis.com/auth/cloud-platform",
-]
+
+#PROJECT_ID = "uk-online-retail-data-project"   # <-- confirm your project id
+#SCOPES = [
+#   "https://www.googleapis.com/auth/bigquery",
+#    "https://www.googleapis.com/auth/cloud-platform",
+#]
 
 # 1) Get user credentials via browser; token is cached locally for reuse
 #creds = pydata_google_auth.get_user_credentials(SCOPES)
@@ -25,10 +26,46 @@ SCOPES = [
 #print(df.head())
 #f.to_csv("full_features_raw.csv", index=False)
 
+####################################### ANALYSIS ########################################
+
 import pandas as pd
 from pathlib import Path
 
-CSV_PATH = Path("/Users/arguncankatergun/uk_analiz_python/uk-online-retail/full_features_raw.csv")
+CSV_PATH = Path("data/full_features_raw.csv")
 df = pd.read_csv(CSV_PATH)
-print(df.shape, df.dtypes.head())
-print(df.head())
+
+#print("Data shape:", df.shape)
+#print("Columns:", df.columns.tolist())
+#print("Missing values per column:\n", df.isnull().sum())
+#print("Sample data:\n", df.head())
+
+#Columns: ['InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate', 'UnitPrice', 'CustomerID', 'Country', 'category', 'InvoiceDateOnly', 'Year', 'Month', 'Day', 'DayOfWeekNum', 'IsWeekend', 'DayOfWeekName', 'MonthName', 'Season', 'IsReturn', 'TotalPrice']
+
+# Further analysis code would go here
+
+########################## RFM Analysis ###########################
+
+df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+df['CustomerID'] = df['CustomerID'].astype(str)
+
+# Filter out rows with missing CustomerID
+df_valid = df[df['CustomerID'].notnull()].copy()
+
+# Convert to string to avoid dtype issues later
+df_valid['CustomerID'] = df_valid['CustomerID'].astype(str)
+
+# Reference date (day after the last transaction)
+snapshot_date = df_valid['InvoiceDate'].max() + pd.Timedelta(days=1)
+
+# Compute RFM
+
+rfm = df_valid.groupby('CustomerID').agg({
+    'InvoiceDate': lambda x: (snapshot_date - x.max()).days,   # Recency
+    'InvoiceNo': 'nunique',                                   # Frequency
+    'TotalPrice': 'sum'                                       # Monetary
+}).rename(columns={'InvoiceDate': 'Recency', 'InvoiceNo': 'Frequency', 'TotalPrice': 'Monetary'})
+
+print("RFM head:\n", rfm.head())
+
+# Save RFM to CSV
+rfm.to_csv("data/rfm_analysis.csv")
